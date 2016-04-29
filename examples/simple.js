7936170fc18425b5af35ad0ef14a1e1b828018ac
tunnckoCore <mameto_100@mail.bg>
2016-04-29 04:52:51 +0300

    add simple example in `examples/` dir

---
+++ examples/simple.js
@@ -0,0 +1,47 @@
+/*!
+ * limon <https://github.com/limonjs/limon>
+ *
+ * Copyright (c) 2016 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+var limon = require('../index')
+
+limon
+  .use(function (app) {
+    return function (ch, i, input) {
+      if (/\s/.test(ch)) {
+        this.tokens.push(['whitespace', ch, i])
+        return
+      }
+      if (/\W/.test(ch)) {
+        this.tokens.push(['symbol', ch, i])
+        return
+      }
+      if (/\d/.test(ch)) {
+        this.tokens.push(['digit', ch, i])
+        return
+      }
+      this.tokens.push(['letter', ch, i])
+    }
+  })
+
+var tokens = limon.tokenize('a > (b + 2)')
+
+console.log(tokens)
+// =>
+// [
+//   [ 'letter', 'a', 0 ],
+//   [ 'whitespace', ' ', 1 ],
+//   [ 'symbol', '>', 2 ],
+//   [ 'whitespace', ' ', 3 ],
+//   [ 'symbol', '(', 4 ],
+//   [ 'letter', 'b', 5 ],
+//   [ 'whitespace', ' ', 6 ],
+//   [ 'symbol', '+', 7 ],
+//   [ 'whitespace', ' ', 8 ],
+//   [ 'digit', '2', 9 ],
+//   [ 'symbol', ')', 10 ]
+// ]
